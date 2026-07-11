f5249ad4f44be6d96da58d55de9d41c2c8353ef7
Nicholas Rodine <nicholas@arnica.io>
2026-07-11 13:13:47 -0400

    Initial commit

---
+++ .nvim.lua
@@ -0,0 +1,9 @@
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "cs" },
+  callback = function()
+    vim.opt_local.expandtab = false
+    vim.opt_local.tabstop = 4
+    vim.opt_local.shiftwidth = 4
+    vim.opt_local.softtabstop = 0
+  end,
+})
