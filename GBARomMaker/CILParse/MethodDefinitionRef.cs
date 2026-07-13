8974e71fe4b41eb1dd5eecb12da5a206e03f89a4
Nicholas Rodine <nicholas@arnica.io>
2026-07-13 17:26:47 -0400

    Started parsing the CIL

---
+++ GBARomMaker/CILParse/MethodDefinitionRef.cs
@@ -0,0 +1,22 @@
+using System.Reflection.Metadata;
+using System.Reflection.PortableExecutable;
+
+public class MethodDefinitionRef {
+	private readonly PEReader peReader;
+	private readonly MetadataReader metadata;
+	private readonly MethodDefinition method;
+	
+	private TypeDefinition parent => metadata.GetTypeDefinition(method.GetDeclaringType());
+
+	public MethodDefinitionRef(PEReader peReader, MetadataReader metadata, MethodDefinition method) {
+		this.peReader = peReader;
+		this.metadata = metadata;
+		this.method = method;
+	}
+
+	public string Namespace => metadata.GetString(parent.Namespace);
+	public string Class => metadata.GetString(parent.Name);
+	public string Name => metadata.GetString(method.Name);
+
+	public byte[] BodyBytes => peReader.GetMethodBody(method.RelativeVirtualAddress).GetILBytes();
+}
--- GBARomMaker/Program.cs
+++ GBARomMaker/Program.cs
@@ -3,6 +3,10 @@ using System.IO;
 using System.Linq;
 using GBARomMaker.Rom;
 using GBARomMaker.Compilation;
+using System.Reflection.Metadata;
+using System.Reflection.Emit;
+using System.Reflection.PortableExecutable;
+using System.Reflection.Metadata.Ecma335;
 
 namespace GBARomMaker;
 public static class Program {
@@ -39,6 +43,82 @@ public static class Program {
 		Console.WriteLine("Hello Red");
 		Console.WriteLine(inputAssembly);
 		Console.WriteLine(outputRom);
+
+		var OpCodesByValue =
+		    typeof(OpCodes)
+        	.GetFields()
+	        .Where(f => f.FieldType == typeof(OpCode))
+	        .Select(f => (OpCode)f.GetValue(null)!)
+	        .ToDictionary(op => unchecked((ushort)op.Value));
+
+		using var stream = File.OpenRead(inputAssembly);
+		using var peReader = new PEReader(stream);
+		var metadata = peReader.GetMetadataReader();
+
+		var entrypoint = DetectEntryPoint(peReader, metadata);
+
+		Console.WriteLine($"Entrypoint: {entrypoint.Namespace}.{entrypoint.Class}.{entrypoint.Name}");
+		Console.WriteLine(string.Join(" ", entrypoint.BodyBytes.Select(b => $"{b:X2}")));
+		try {
+			PrintCIL(entrypoint.BodyBytes);
+		}
+		catch (Exception ex) {
+			Console.Error.WriteLine(ex.Message);
+		}
+		//foreach (TypeDefinitionHandle typeHandle in metadata.TypeDefinitions)
+		//{
+		//	TypeDefinition type = metadata.GetTypeDefinition(typeHandle);
+
+		//	var namespaceName = metadata.GetString(type.Namespace);
+		//	var typeName = metadata.GetString(type.Name);
+
+		//	Console.WriteLine($"Type: {namespaceName}.{typeName}");
+		//	foreach (MethodDefinitionHandle methodHandle in type.GetMethods())
+		//	{
+		//		MethodDefinition method = metadata.GetMethodDefinition(methodHandle);
+		//		var methodName = metadata.GetString(method.Name);
+		//		Console.WriteLine($"  Method: {methodName}");
+
+		//		if (method.RelativeVirtualAddress == 0)
+		//			continue; // Abstract, extern, etc.
+
+		//		MethodBodyBlock body = peReader.GetMethodBody(method.RelativeVirtualAddress);
+
+		//		byte[] ilBytes = body.GetILBytes().ToArray();
+
+		//		Console.WriteLine($"    IL: {string.Join(" ", ilBytes.Select(b => $"{b:X2}"))}");
+		//	}
+		//}
+
 		return 0;
+	}
+
+	public static MethodDefinitionRef DetectEntryPoint(PEReader peReader, MetadataReader metadata) {
+		var corHeader = peReader.PEHeaders.CorHeader ?? throw new InvalidDataException("Not a managed assembly.");
+		var entryPointToken = corHeader.EntryPointTokenOrRelativeVirtualAddress;
+		var entryPointHandle = MetadataTokens.EntityHandle(entryPointToken);
+		if (entryPointHandle.Kind != HandleKind.MethodDefinition) throw new InvalidDataException("Entry point is not a managed method.");
+
+		var methodHandle = (MethodDefinitionHandle)entryPointHandle;
+		var method = metadata.GetMethodDefinition(methodHandle);
+		return new MethodDefinitionRef(peReader, metadata, method);
+	}
+
+	public static void PrintCIL(byte[] data) {
+		// https://www.ecma-international.org/wp-content/uploads/ECMA-335_6th_edition_june_2012.pdf
+		for (var i = 0; i < data.Count(); i++) {
+			var op = data[i];
+			switch (op) {
+				case 0x00:
+					Console.WriteLine("0x00 NOP");
+					break;
+				case 0x20:
+					var value = BitConverter.ToInt32(data[(i+1)..(i+4)]);
+					i += 4;
+					Console.WriteLine($"0x20 ldc.i4 0x{value:X8}");
+					break;
+				default: throw new NotImplementedException($"0x{op:X2}");
+			}
+		}
 	}
 }
