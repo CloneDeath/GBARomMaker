d003093007c4b67a5c538de1a26702a901084371
Nicholas Rodine <nicholas@arnica.io>
2026-07-18 07:58:13 -0400

    Split the red pixel example into 2 projects, line and pixel

--- Examples/RedPixel/Program.cs
+++ Examples/RedPixel/Program.cs
@@ -1,16 +1,5 @@
 ﻿unsafe
 {
-    ushort* displayControl = (ushort*)0x04000000;
-    *displayControl = 0x0403; // Mode 3 + BG2 enabled
-
-    ushort* topLeftPixel = (ushort*)0x06000000;
-	for (var i = 0; i < 10; i++) {
-    	*(topLeftPixel+i) = 0x001F; // Red
-	}
+    *(ushort*)0x04000000 = 0x0403;
+    *(ushort*)0x06000000 = 0x001F;
 }
-
-//unsafe
-//{
-//    *(ushort*)0x04000000 = 0x0403;
-//    *(ushort*)0x06000000 = 0x001F;
-//}
