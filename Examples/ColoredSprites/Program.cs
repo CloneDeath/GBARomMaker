33df31b9f26a65046e752643675510bb40b96f8c
Nicholas Rodine <nicholas@arnica.io>
2026-07-18 08:01:00 -0400

    Started work on a colored sprites project

---
+++ Examples/ColoredSprites/Program.cs
@@ -0,0 +1,10 @@
+﻿unsafe
+{
+    ushort* displayControl = (ushort*)0x04000000;
+    *displayControl = 0x0403; // Mode 3 + BG2 enabled
+
+    ushort* topLeftPixel = (ushort*)0x06000000;
+	for (var i = 0; i < 10; i++) {
+    	*(topLeftPixel+i) = 0x001F; // Red
+	}
+}
