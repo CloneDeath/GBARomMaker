99e39df5fc3692b235f0be296a8df028760b5086
Nicholas Rodine <nicholas@arnica.io>
2026-07-20 16:04:43 -0400

    Implemented a 'red square' program, with a callable function with args

--- Examples/ColoredSprites/Program.cs
+++ Examples/ColoredSprites/Program.cs
@@ -2,15 +2,22 @@
 {
 	DisplayController.EnableMode3AndBG2();
 
-    ushort* topLeftPixel = (ushort*)0x06000000;
-	for (var i = 0; i < 10; i++) {
-    	*(topLeftPixel+i) = 0x001F; // Red
+	for (var i = 0; i <= 10; i++) {
+		DisplayController.SetPixelRed(i, 0);
+		DisplayController.SetPixelRed(i, 10);
+		DisplayController.SetPixelRed(0, i);
+		DisplayController.SetPixelRed(10, i);
 	}
 }
 
-public static class DisplayController {
+public static unsafe class DisplayController {
 	public static unsafe void EnableMode3AndBG2() {
     	ushort* displayControl = (ushort*)0x04000000;
 	    *displayControl = 0x0403; // Mode 3 + BG2 enabled
+	}
+
+	public static void SetPixelRed(int x, int y) {
+    	ushort* topLeftPixel = (ushort*)0x06000000;
+    	*(topLeftPixel+x+(y*240)) = 0x001F; // Red
 	}
 }
