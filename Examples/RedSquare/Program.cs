c7ed06d87bab9d033235ff0e4fff601fb8984681
Nicholas Rodine <nicholas@arnica.io>
2026-07-21 14:44:36 -0400

    Added red square example

---
+++ Examples/RedSquare/Program.cs
@@ -0,0 +1,23 @@
+﻿unsafe
+{
+	DisplayController.EnableMode3AndBG2();
+
+	for (var i = 0; i <= 10; i++) {
+		DisplayController.SetPixelRed(i, 0);
+		DisplayController.SetPixelRed(i, 10);
+		DisplayController.SetPixelRed(0, i);
+		DisplayController.SetPixelRed(10, i);
+	}
+}
+
+public static unsafe class DisplayController {
+	public static void EnableMode3AndBG2() {
+		ushort* displayControl = (ushort*)0x04000000;
+		*displayControl = 0x0403; // Mode 3 + BG2 enabled
+	}
+
+	public static void SetPixelRed(int x, int y) {
+		ushort* topLeftPixel = (ushort*)0x06000000;
+		*(topLeftPixel+x+(y*240)) = 0x001F; // Red
+	}
+}
--- build-examples.sh
+++ build-examples.sh
@@ -4,4 +4,5 @@ set -euo pipefail
 dotnet build
 dotnet "GBARomMaker/bin/Debug/net10.0/GBARomMaker.dll" "Examples/RedPixel/bin/Debug/net10.0/RedPixel.dll" "Examples/RedPixel.gba"
 dotnet "GBARomMaker/bin/Debug/net10.0/GBARomMaker.dll" "Examples/RedLine/bin/Debug/net10.0/RedLine.dll" "Examples/RedLine.gba"
+dotnet "GBARomMaker/bin/Debug/net10.0/GBARomMaker.dll" "Examples/RedLine/bin/Debug/net10.0/RedSquare.dll" "Examples/RedSquare.gba"
 dotnet "GBARomMaker/bin/Debug/net10.0/GBARomMaker.dll" "Examples/ColoredSprites/bin/Debug/net10.0/ColoredSprites.dll" "Examples/ColoredSprites.gba" --show-cil --show-arm
