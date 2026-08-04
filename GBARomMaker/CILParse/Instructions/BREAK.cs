61ee51e6a39af34f5c043515d1b76505837ae8e1
Nicholas Rodine <nicholas@arnica.io>
2026-08-04 04:33:47 -0400

    Added DIV and BREAK CIL instructions

---
+++ GBARomMaker/CILParse/Instructions/BREAK.cs
@@ -0,0 +1,26 @@
+using System.Collections.Generic;
+using System.Reflection.Emit;
+using System.Reflection.Metadata;
+using GBARomMaker.CIL;
+
+namespace GBARomMaker.CILParse.Instructions;
+
+public class BREAK : CILInstruction {
+	public static CILInstructionDefinition Definition = new(0x01, 0, (_) => new BREAK());
+
+	public OpCode OpCode => OpCodes.Break;
+
+    public byte[] GetBytes() {
+		return [0x01];
+    }
+
+    public string GetCIL(CILFactory factory, ICILMethod method) {
+		return "break";
+    }
+
+	public void ModifyStack(CILFactory factory, ICILMethod method, Stack<SignatureTypeCode> current) {
+	}
+    public bool AlwaysBranches => false;
+	public bool SometimesBranches => false;
+	public int? BranchTarget => null;
+}
---
+++ GBARomMaker/CILParse/Instructions/DIV.cs
@@ -0,0 +1,29 @@
+using System.Collections.Generic;
+using System.Reflection.Emit;
+using System.Reflection.Metadata;
+using GBARomMaker.CIL;
+
+namespace GBARomMaker.CILParse.Instructions;
+
+public class DIV : CILInstruction {
+	public static CILInstructionDefinition Definition = new(0x5B, 0, (_) => new DIV());
+
+	public OpCode OpCode => OpCodes.Div;
+
+    public byte[] GetBytes() {
+		return [0x5B];
+    }
+
+    public string GetCIL(CILFactory factory, ICILMethod method) {
+		return "div";
+    }
+
+	public void ModifyStack(CILFactory factory, ICILMethod method, Stack<SignatureTypeCode> current) {
+		var type = current.Pop();
+		current.Pop();
+		current.Push(type);
+	}
+    public bool AlwaysBranches => false;
+	public bool SometimesBranches => false;
+	public int? BranchTarget => null;
+}
