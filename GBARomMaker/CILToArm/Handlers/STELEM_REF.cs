12570ed38f416c480b57e28958e681c32e7866bd
Nicholas Rodine <nicholas@arnica.io>
2026-08-08 07:16:45 -0400

    did an lsl instead of a multiply when getting array index

--- GBARomMaker/CILToArm/Handlers/STELEM_REF.cs
+++ GBARomMaker/CILToArm/Handlers/STELEM_REF.cs
@@ -9,10 +9,8 @@ public class STELEM_REF : ICILToArmHandler {
 	public ArmCode Handle(InstructionMetadata instruction) {
 		return new ArmCode([
 			"pop sp!, { r0, r1, r2 } @ value, index, array",
-			"ldr r3, =4",
-			"mul r1, r1, r3",
-			"add r1, r1, #4 @ first word is array length",
-			"str r2, [r0, r1]"
+			"add r1, r1, #1 @ first word is array length",
+			"str r2, [r0, r1, lsl #2]"
 		]);
 	}
 }
