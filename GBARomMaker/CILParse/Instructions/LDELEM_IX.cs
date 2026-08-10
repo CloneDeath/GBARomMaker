d61eba98c4b622fc06892de673f63b36c5b0e91b
Nicholas Rodine <nicholas@arnica.io>
2026-08-10 04:46:17 -0400

    fixed STELEM_IX, and added LDELEM_I/UX

--- GBARomMaker/CILParse/Instructions/LDELEM_IX.cs
+++ GBARomMaker/CILParse/Instructions/LDELEM_IX.cs
@@ -1,50 +1,57 @@
 using System;
 using System.Collections.Generic;
 using System.Reflection.Emit;
+using System.Reflection.Metadata;
 using GBARomMaker.CIL;
 
 namespace GBARomMaker.CILParse.Instructions;
 
-public class STELEM_IX : CILInstruction {
+public class LDELEM_IX : CILInstruction {
 	public static CILInstructionDefinition[] Definitions = [
-		new(0x9C, 0, (_) => new STELEM_IX(1)),
-		new(0x9D, 0, (_) => new STELEM_IX(2)),
-		new(0x9E, 0, (_) => new STELEM_IX(4)),
-		new(0x9F, 0, (_) => new STELEM_IX(8))
+		new(0x90, 0, (_) => new LDELEM_IX(1)),
+		new(0x92, 0, (_) => new LDELEM_IX(2)),
+		new(0x94, 0, (_) => new LDELEM_IX(4)),
+		new(0x96, 0, (_) => new LDELEM_IX(8))
 	];
 
-	public STELEM_IX(int bytes) {
+	public LDELEM_IX(int bytes) {
 		Bytes = bytes;
 	}
 
 	public int Bytes { get; }
 
 	public OpCode OpCode => Bytes switch {
-		1 => OpCodes.Stelem_I1,
-		2 => OpCodes.Stelem_I2,
-		4 => OpCodes.Stelem_I4,
-		8 => OpCodes.Stelem_I8,
+		1 => OpCodes.Ldelem_I1,
+		2 => OpCodes.Ldelem_I2,
+		4 => OpCodes.Ldelem_I4,
+		8 => OpCodes.Ldelem_I8,
 		_ => throw new Exception("Invalid number of bytes")
 	};
 
     public byte[] GetBytes() {
 		return [Bytes switch {
-			1 => 0x9C,
-			2 => 0x9D,
-			4 => 0x9E,
-			8 => 0x9F,
+			1 => 0x90,
+			2 => 0x92,
+			4 => 0x94,
+			8 => 0x96,
 			_ => throw new Exception("Invalid number of bytes")
 		}];
     }
 
     public string GetCIL(CILFactory factory, ICILMethod method) {
-		return $"stelem.i{Bytes}";
+		return $"ldelem.i{Bytes}";
     }
 
 	public void ModifyStack(CILFactory factory, ICILMethod method, Stack<ISignatureType> current) {
 		current.Pop();
 		current.Pop();
-		current.Pop();
+		current.Push(new SignatureType(Bytes switch {
+			1 => SignatureTypeCode.Byte,
+			2 => SignatureTypeCode.Int16,
+			4 => SignatureTypeCode.Int32,
+			8 => SignatureTypeCode.Int64,
+			_ => throw new Exception("Invalid number of bytes")
+		}));
 	}
 
     public bool AlwaysBranches => false;
--- GBARomMaker/CILParse/Instructions/LDELEM_UX.cs
+++ GBARomMaker/CILParse/Instructions/LDELEM_UX.cs
@@ -1,50 +1,54 @@
 using System;
 using System.Collections.Generic;
 using System.Reflection.Emit;
+using System.Reflection.Metadata;
 using GBARomMaker.CIL;
 
 namespace GBARomMaker.CILParse.Instructions;
 
-public class STELEM_IX : CILInstruction {
+public class LDELEM_UX : CILInstruction {
 	public static CILInstructionDefinition[] Definitions = [
-		new(0x9C, 0, (_) => new STELEM_IX(1)),
-		new(0x9D, 0, (_) => new STELEM_IX(2)),
-		new(0x9E, 0, (_) => new STELEM_IX(4)),
-		new(0x9F, 0, (_) => new STELEM_IX(8))
+		new(0x91, 0, (_) => new LDELEM_UX(1)),
+		new(0x93, 0, (_) => new LDELEM_UX(2)),
+		new(0x95, 0, (_) => new LDELEM_UX(4)),
+		// new(0x96, 0, (_) => new LDELEM_UX(8)) ALIAS for ldelem.i8
 	];
 
-	public STELEM_IX(int bytes) {
+	public LDELEM_UX(int bytes) {
 		Bytes = bytes;
 	}
 
 	public int Bytes { get; }
 
 	public OpCode OpCode => Bytes switch {
-		1 => OpCodes.Stelem_I1,
-		2 => OpCodes.Stelem_I2,
-		4 => OpCodes.Stelem_I4,
-		8 => OpCodes.Stelem_I8,
+		1 => OpCodes.Ldelem_U1,
+		2 => OpCodes.Ldelem_U2,
+		4 => OpCodes.Ldelem_U4,
 		_ => throw new Exception("Invalid number of bytes")
 	};
 
     public byte[] GetBytes() {
 		return [Bytes switch {
-			1 => 0x9C,
-			2 => 0x9D,
-			4 => 0x9E,
-			8 => 0x9F,
+			1 => 0x91,
+			2 => 0x93,
+			4 => 0x95,
 			_ => throw new Exception("Invalid number of bytes")
 		}];
     }
 
     public string GetCIL(CILFactory factory, ICILMethod method) {
-		return $"stelem.i{Bytes}";
+		return $"ldelem.u{Bytes}";
     }
 
 	public void ModifyStack(CILFactory factory, ICILMethod method, Stack<ISignatureType> current) {
 		current.Pop();
 		current.Pop();
-		current.Pop();
+		current.Push(new SignatureType(Bytes switch {
+			1 => SignatureTypeCode.Byte,
+			2 => SignatureTypeCode.UInt16,
+			4 => SignatureTypeCode.UInt32,
+			_ => throw new Exception("Invalid number of bytes")
+		}));
 	}
 
     public bool AlwaysBranches => false;
