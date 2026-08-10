65851ce28f9d1d5e6715ba60d798b2203dd52ed5
Nicholas Rodine <nicholas@arnica.io>
2026-08-10 04:46:42 -0400

    Uncommented build for arrays, starting with a simple int list first

--- Examples/07-SpriteArray/Program.cs
+++ Examples/07-SpriteArray/Program.cs
@@ -3,11 +3,17 @@ using GBA;
 
 unsafe {
 	DisplayController.EnableVBlank();
-	DisplayController.SetControl(new DisplayControl {
-		BGMode = 3,
-		ScreenDisplayBG2 = true,
-		ScreenDisplayOBJ = true
-	});
+	//DisplayController.SetControl(new DisplayControl {
+	//	BGMode = 3,
+	//	ScreenDisplayBG2 = true,
+	//	ScreenDisplayOBJ = true
+	//});
+
+	var x = new int[1] { 12 };
+	Console.WriteLine(x[0]);
+	while (true) {
+		Interrupt.WaitVBlank();
+	}
 
 	//Sprite[] sprites = new Sprite[30];
 	//for (byte i = 0; i < sprites.Length; i++) {
@@ -27,29 +33,31 @@ unsafe {
 	//	};
 	//}
 	
-	Palette.SetObject(0, 0, Colors.Red);
-	Palette.SetObject(0, 1, Colors.Green);
-	Palette.SetObject(0, 2, Colors.Blue);
-	for (byte y = 0; y < 8; y++) {
-	for (byte x = 0; x < 8; x++) {
-		CharacterData.SetColor(513, x, y, 1);
-	}
-	}
+	//Palette.SetObject(0, 0, Colors.Red);
+	//Palette.SetObject(0, 1, Colors.Green);
+	//Palette.SetObject(0, 2, Colors.Blue);
+	//for (byte y = 0; y < 8; y++) {
+	//for (byte x = 0; x < 8; x++) {
+	//	CharacterData.SetColor(513, x, y, 1);
+	//}
+	//}
 
-	var sprites = new Sprite[1];
+	//var sprites = new Sprite[1];
+	//
+	//Console.WriteLine("begin");
+	//sprites[0] = new Sprite(0) {
+	//	TileIndex = 513,
+	//	Y = 76
+	//};
 
-	sprites[0] = new Sprite(0) {
-		TileIndex = 513,
-		Y = 76
-	};
-
-	int timer = 0;
-	while(true){
-		timer += 1;
-		sprites[0].Y = 76;
-		sprites[0].X = (byte)(timer%232);
-		Interrupt.WaitVBlank();
-	};
+	//int timer = 0;
+	//Console.WriteLine(sprites[0].Y);
+	//while(true){
+	//	timer += 1;
+	//	sprites[0].Y = 76;
+	//	sprites[0].X = (byte)(timer%232);
+	//	Interrupt.WaitVBlank();
+	//};
 }
 
 static Color ColorFromHSV(float hue, float saturation, float value)
