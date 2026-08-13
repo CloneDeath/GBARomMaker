60822207aafc184d9594995584bcd7b3e7891345
Nicholas Rodine <nicholas@arnica.io>
2026-08-13 05:54:21 -0400

    Updated example. We'll have to support the interpolation at another date...

--- Examples/06-ConsoleWrite/Program.cs
+++ Examples/06-ConsoleWrite/Program.cs
@@ -1,13 +1,12 @@
 ﻿using System;
 using GBA;
-
 unsafe {
 	Console.WriteLine("hello world");
 	Console.WriteLine(12);
 	Console.WriteLine("h" + 1);
+	//Console.WriteLine($"\tis not {1}.");
 	for (var i = 0; i < 10; i++) {
 		Console.WriteLine("value " + i);
-		//Console.WriteLine($"\tis not {i+1}.");
 	}
 	DisplayController.EnableVBlank();
 	while (true) {
