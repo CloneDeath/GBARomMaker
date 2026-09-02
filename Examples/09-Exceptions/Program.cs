5537681c166fe0a15664fe180dd0b7c45a81d574
Nicholas Rodine <nicholas@arnica.io>
2026-09-02 05:33:09 -0400

    Added an exceptions example

---
+++ Examples/09-Exceptions/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+unsafe
+{
+    *(ushort*)0x04000000 = 0x0403;
+	try {
+		throw new Exception("Error");
+	}
+	catch (Exception e) {
+		Console.WriteLine(e.Message);
+    	*(ushort*)0x06000000 = 0x03E0;
+	}
+}
